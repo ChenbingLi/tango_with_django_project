cdad0dd31d5a12d551993fb870486286db46a5e9
Chenbing Li <2534967L@student.gla.ac.uk>
2021-07-30 18:32:38 +0800

    final commit

--- rango/forms.py
+++ rango/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from rango.models import Page, Category
 from rango.models import UserProfile
-# We could add these forms to views.py, but it makes sense to split them off into their own file.
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
